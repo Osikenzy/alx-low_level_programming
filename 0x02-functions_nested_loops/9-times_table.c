b89add222113e1aabe7863b3dafe74a0ac8abf32
Osikenzy <osikenzy@gmail.com>
2022-04-12 15:09:22 -0700

    my repo

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,6 @@
 
 void times_table(void)
 {
-
 int a = 0;
 int b;	
 int rep;
@@ -31,7 +30,8 @@ else
 _putchar('0' + rep / 10);
 _putchar('0' + rep % 10);
 }
-if (b < 9){
+if (b < 9)
+{
 _putchar(',');
 _putchar(' ');
 }
