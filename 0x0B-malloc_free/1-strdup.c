3d6c46189cb30538ce5f571748a714273e1909ef
Osikenzy <osikenzy@gmail.com>
2022-05-07 04:50:13 +0100

    my repo

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,8 +4,10 @@
 /**
  * _strdup - returns a pointer to a newly allocated space in memory.
  * @str: string.
+ *
  * Return: pointer of an array of chars
  */
+
 char *_strdup(char *str)
 {
 char *strout;
